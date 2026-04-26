68bb97c5c875b246d6f303473c083a4eabe10694
kakuraccho <kakurattyo1216@gmail.com>
2026-04-26 23:26:14 +0900

    add: docker

---
+++ backend/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("api/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "correct to connect backend",
+		})
+	})
+	router.Run()
+}
